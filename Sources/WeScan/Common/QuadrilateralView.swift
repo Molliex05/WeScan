7626695dfb583530ca3d94b05bd0b2309c98aec2
Molliex05 <molliex05@icloud.com>
2025-08-11 11:54:56 -0400

    Update QuadrilateralView.swift

--- Sources/WeScan/Common/QuadrilateralView.swift
+++ Sources/WeScan/Common/QuadrilateralView.swift
@@ -47,8 +47,8 @@ final class QuadrilateralView: UIView {
           didSet {
               cornerViews(hidden: !editable)
               quadLayer.fillColor = editable
-                  ? UIColor.systemOrange.withAlphaComponent(0.25).cgColor   // overlay orange (édition)
-                  : UIColor.systemOrange.withAlphaComponent(0.15).cgColor   // overlay orange (normal)
+                  ? UIColor.systemOrange.withAlphaComponent(0.45).cgColor   // overlay orange (édition)
+                  : UIColor.systemOrange.withAlphaComponent(0.35).cgColor   // overlay orange (normal)
               guard let quad else { return }
               drawQuad(quad, animated: false)
              layoutCornerViews(forQuad: quad)
@@ -67,20 +67,18 @@ final class QuadrilateralView: UIView {
     }
 
     private var isHighlighted = false {
-        didSet (oldValue) {
-            guard oldValue != isHighlighted else {
-                return
-            }
-            quadLayer.fillColor = isHighlighted
-             ? UIColor.clear.cgColor
-             : UIColor.systemOrange.withAlphaComponent(0.20).cgColor
-            if isHighlighted {
-                bringSubviewToFront(quadView)
-            } else {
-                sendSubviewToBack(quadView)
-            }
-        }
-    }
+          didSet(oldValue) {
+              guard oldValue != isHighlighted else { return }
+              quadLayer.fillColor = isHighlighted
+                  ? UIColor.clear.cgColor
+                  : UIColor.systemOrange.withAlphaComponent(0.45).cgColor
+              if isHighlighted {
+                  bringSubviewToFront(quadView)
+              } else {
+                 sendSubviewToBack(quadView)
+             }
+         }
+     }
 
     private lazy var topLeftCornerView: EditScanCornerView = {
         return EditScanCornerView(frame: CGRect(origin: .zero, size: cornerViewSize), position: .topLeft)
